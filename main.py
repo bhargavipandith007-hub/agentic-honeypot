7362cefcc8a8cc535613147fb85622e870b06284
Pandith Bhargavi <bhargavipandith007@gmail.com>
2026-02-05 23:05:07 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,14 +2,9 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/")
-def root():
-    return {"status": "API is running"}
-
-@app.post("/analyze")
-def analyze(data: dict):
-    message = data.get("message", "")
+@app.api_route("/honeypot/analyze", methods=["GET", "POST"])
+def honeypot():
     return {
-        "received_message": message,
-        "scam_detected": "kyc" in message.lower()
+        "status": "success",
+        "reply": "I am confused. Can you explain why my account is blocked?"
     }
